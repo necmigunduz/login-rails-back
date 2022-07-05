467ed2169b56ae1a7ac02dceb7a262b61bfa50f9
necmigunduz <necmigunduz@gmail.com>
2022-07-05 15:38:20 +0300

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:create, :show, :index] do
+    resources :items, only: [:create, :show, :index, :destroy]
+  end
 end
